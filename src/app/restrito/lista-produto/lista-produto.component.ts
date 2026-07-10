9732e654db9ec3a176777c49664136587da9a25c
MatZe <MatZeraH64@hotmail.com>
2026-07-10 01:40:51 -0300

    Sei que é falta de profissionalismo, mas estou com preguiça de fazer uma mensagem de commit para cada um dos arquivos

---
+++ src/app/restrito/lista-produto/lista-produto.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Produto } from 'src/app/models/Produto.model';
+import { ProdutoService } from 'src/app/produto.service';
+import { LoginService } from 'src/app/login.service';
+
+@Component({
+  selector: 'app-lista-produto',
+  templateUrl: './lista-produto.component.html',
+  styleUrls: ['./lista-produto.component.css']
+})
+export class ListaProdutoComponent implements OnInit {
+
+  public produtos: Produto[] = [];
+
+  constructor(
+    private _produtoService: ProdutoService,
+    private router: Router,
+    private _loginService: LoginService) { }
+
+  ngOnInit(): void {
+    this.listarProdutos();
+    this._loginService.setMostraMenu(false);
+  }
+
+  listarProdutos(): void {
+    this._produtoService.getProdutos()
+      .subscribe(
+        retornaProduto => {
+          this.produtos = retornaProduto.map(
+            item => {
+              return new Produto(
+                item.id,
+                item.produto,
+                item.descricao,
+                item.foto,
+                item.preco
+              );
+            }
+          );
+        }
+      );
+  }
+
+  excluir(id: number) {
+    this._produtoService.removerProduto(id).subscribe(
+      produto => {
+        this.listarProdutos();
+      },
+      err => { console.log("erro ao excluir"); }
+    );
+    this.router.navigate(["/restrito/lista"]);
+  }
+}
